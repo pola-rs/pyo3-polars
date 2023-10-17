4500d8d568eb632a62a8ef90296c7be9ceabe3e5
Jakob Kraus <mail@jakobkraus.de>
2023-10-17 16:29:47 +0200

    Implement FromPyObject and IntoPy for AnyValue

--- pyo3-polars/src/lib.rs
+++ pyo3-polars/src/lib.rs
@@ -1,4 +1,4 @@
-//! This crate offers a [`PySeries`] and a [`PyDataFrame`] which are simple wrapper around `Series` and `DataFrame`. The
+//! This crate offers [`PySeries`], [`PyDataFrame`] and [`PyAnyValue`] which are simple wrapper around `Series`, `DataFrame` and `PyAnyValue`. The
 //! advantage of these wrappers is that they can be converted to and from python as they implement `FromPyObject` and `IntoPy`.
 //!
 //! # Example
@@ -51,7 +51,7 @@ mod ffi;
 use crate::error::PyPolarsErr;
 use crate::ffi::to_py::to_py_array;
 use polars::prelude::*;
-use pyo3::{FromPyObject, IntoPy, PyAny, PyObject, PyResult, Python, ToPyObject};
+use pyo3::{FromPyObject, IntoPy, PyAny, PyObject, PyResult, Python, ToPyObject, types::{PyDelta, PyNone, PyDate, PyDateTime, PyTime}, exceptions::PyTypeError};
 
 #[cfg(feature = "lazy")]
 use {polars_lazy::frame::LazyFrame, polars_plan::logical_plan::LogicalPlan};
@@ -65,6 +65,11 @@ pub struct PySeries(pub Series);
 #[derive(Debug, Clone)]
 /// A wrapper around a [`DataFrame`] that can be converted to and from python with `pyo3`.
 pub struct PyDataFrame(pub DataFrame);
+
+#[repr(transparent)]
+#[derive(Debug, Clone)]
+/// A wrapper around a [`AnyValue`] that can be converted to and from python with `pyo3`.
+pub struct PyAnyValue<'a>(pub AnyValue<'a>);
 
 #[cfg(feature = "lazy")]
 #[repr(transparent)]
@@ -90,6 +95,12 @@ impl From<PySeries> for Series {
     }
 }
 
+impl<'a> From<PyAnyValue<'a>> for AnyValue<'a> {
+    fn from(value: PyAnyValue<'a>) -> Self {
+        value.0
+    }
+}
+
 #[cfg(feature = "lazy")]
 impl From<PyLazyFrame> for LazyFrame {
     fn from(value: PyLazyFrame) -> Self {
@@ -105,6 +116,12 @@ impl AsRef<Series> for PySeries {
 
 impl AsRef<DataFrame> for PyDataFrame {
     fn as_ref(&self) -> &DataFrame {
+        &self.0
+    }
+}
+
+impl<'a> AsRef<AnyValue<'a>> for PyAnyValue<'a> {
+    fn as_ref(&self) -> &AnyValue<'a> {
         &self.0
     }
 }
@@ -142,6 +159,75 @@ impl<'a> FromPyObject<'a> for PyDataFrame {
             columns.push(s);
         }
         Ok(PyDataFrame(DataFrame::new_no_checks(columns)))
+    }
+}
+
+impl<'a> FromPyObject<'a> for PyAnyValue<'a> {
+    fn extract(ob: &'a PyAny) -> PyResult<Self> {
+        let object_type = ob
+            .getattr("__class__")?
+            .getattr("__name__")?
+            .extract::<&str>()?;
+
+        println!("{}", object_type);
+
+        match object_type {
+            "float" => Ok(PyAnyValue(AnyValue::Float64(ob.extract::<f64>()?))),
+            "int" => Ok(PyAnyValue(AnyValue::Int64(ob.extract::<i64>()?))),
+            "str" => Ok(PyAnyValue(AnyValue::Utf8(ob.extract::<&str>()?))),
+            "bool" => Ok(PyAnyValue(AnyValue::Boolean(ob.extract::<bool>()?))),
+            "datetime" => {
+                let timestamp = (ob.call_method0("timestamp")?.extract::<f64>()? * 1000.0) as i64;
+                Ok(PyAnyValue(AnyValue::Datetime(
+                    timestamp,
+                    TimeUnit::Milliseconds,
+                    &None,
+                )))
+            }
+            "date" => {
+                let days = Python::with_gil(|py| {
+                    let datetime = py
+                        .import("datetime")
+                        .expect("Cannot import datetime module");
+
+                    let epoch = datetime.call_method1("date", (1970, 1, 1)).unwrap();
+
+                    ob.call_method1("__sub__", (epoch,))
+                        .expect("Failed to convert Date object")
+                        .getattr("days")
+                        .expect("Failed to convert Date object")
+                        .extract::<i32>()
+                })?;
+                Ok(PyAnyValue(AnyValue::Date(days)))
+            }
+            "timedelta" => {
+                let seconds = (ob.call_method0("total_seconds")?.extract::<f64>()? * 1000.0) as i64;
+                Ok(PyAnyValue(AnyValue::Duration(
+                    seconds,
+                    TimeUnit::Milliseconds,
+                )))
+            }
+            "time" => {
+                let hours = ob.getattr("hour")?.extract::<i64>()?;
+                let minutes = ob.getattr("minute")?.extract::<i64>()?;
+                let seconds = ob.getattr("second")?.extract::<i64>()?;
+                let microseconds = ob.getattr("microsecond")?.extract::<i64>()?;
+
+                Ok(PyAnyValue(AnyValue::Time(
+                    (hours * 3600000000000)
+                        + (minutes * 60000000000)
+                        + (seconds * 1000000000)
+                        + (microseconds * 1000),
+                )))
+            }
+            "Series" => Ok(PyAnyValue(AnyValue::List(ob.extract::<PySeries>()?.0))),
+            "bytes" => Ok(PyAnyValue(AnyValue::Binary(ob.extract::<&[u8]>()?))),
+            "NoneType" => Ok(PyAnyValue(AnyValue::Null)),
+            _ => Err(PyTypeError::new_err(format!(
+                "'{}' object cannot be interpreted",
+                object_type
+            ))),
+        }
     }
 }
 
@@ -185,6 +271,104 @@ impl IntoPy<PyObject> for PyDataFrame {
         let polars = py.import("polars").expect("polars not installed");
         let df_object = polars.call_method1("DataFrame", (pyseries,)).unwrap();
         df_object.into_py(py)
+    }
+}
+
+macro_rules! convert_duration (
+    ($py:expr, $difference:expr, $second_factor:literal) => {
+        {
+            let days = $difference / ($second_factor * 8640);
+            let remaining_after_days = $difference % ($second_factor * 8640);
+            let seconds = remaining_after_days / $second_factor;
+            let remaining_after_seconds = remaining_after_days % $second_factor;
+            let microseconds = remaining_after_seconds * (1000000 / $second_factor);
+
+            PyDelta::new(
+                $py,
+                i32::try_from(days).unwrap(),
+                i32::try_from(seconds).unwrap(),
+                i32::try_from(microseconds).unwrap(),
+                false,
+            )
+            .unwrap()
+            .into_py($py)
+        }
+    }
+);
+
+impl IntoPy<PyObject> for PyAnyValue<'_> {
+    fn into_py(self, py: Python<'_>) -> PyObject {
+        match self.0 {
+            AnyValue::Binary(val) => val.into_py(py),
+            AnyValue::Null => PyNone::get(py).into_py(py),
+            AnyValue::Boolean(val) => val.into_py(py),
+            AnyValue::Utf8(val) => val.into_py(py),
+            AnyValue::UInt8(val) => val.into_py(py),
+            AnyValue::UInt16(val) => val.into_py(py),
+            AnyValue::UInt32(val) => val.into_py(py),
+            AnyValue::UInt64(val) => val.into_py(py),
+            AnyValue::Int8(val) => val.into_py(py),
+            AnyValue::Int16(val) => val.into_py(py),
+            AnyValue::Int32(val) => val.into_py(py),
+            AnyValue::Int64(val) => val.into_py(py),
+            AnyValue::Float32(val) => val.into_py(py),
+            AnyValue::Float64(val) => val.into_py(py),
+            AnyValue::Date(days) => PyDate::from_timestamp(py, (days * 86400).into())
+                .unwrap()
+                .into_py(py),
+            // The timezone is ignored - This may lead to wrong conversions
+            AnyValue::Datetime(time, unit, _timezone) => match unit {
+                polars::prelude::TimeUnit::Milliseconds => {
+                    PyDateTime::from_timestamp(py, (time / 1000) as f64, None)
+                        .unwrap()
+                        .into_py(py)
+                }
+                polars::prelude::TimeUnit::Microseconds => {
+                    PyDateTime::from_timestamp(py, (time / 1000000) as f64, None)
+                        .unwrap()
+                        .into_py(py)
+                }
+                polars::prelude::TimeUnit::Nanoseconds => {
+                    PyDateTime::from_timestamp(py, (time / 1000000000) as f64, None)
+                        .unwrap()
+                        .into_py(py)
+                }
+            },
+            AnyValue::Duration(difference, unit) => match unit {
+                polars::prelude::TimeUnit::Milliseconds => {
+                    convert_duration!(py, difference, 1000)
+                }
+                polars::prelude::TimeUnit::Microseconds => {
+                    convert_duration!(py, difference, 1000000)
+                }
+                polars::prelude::TimeUnit::Nanoseconds => {
+                    convert_duration!(py, difference, 1000000000)
+                }
+            },
+            AnyValue::Time(nanoseconds) => {
+                let hours = nanoseconds / 3600000000000;
+                let remaining_after_hours = nanoseconds % 3600000000000;
+                let minutes = remaining_after_hours / 60000000000;
+                let remaining_after_minutes = remaining_after_hours % 60000000000;
+                let seconds = remaining_after_minutes / 1000000000;
+                let remaining_after_seconds = remaining_after_minutes % 1000000000;
+                let microseconds = remaining_after_seconds / 1000;
+
+                PyTime::new(
+                    py,
+                    u8::try_from(hours).unwrap(),
+                    u8::try_from(minutes).unwrap(),
+                    u8::try_from(seconds).unwrap(),
+                    u32::try_from(microseconds).unwrap(),
+                    None,
+                )
+                .unwrap()
+                .into_py(py)
+            }
+            AnyValue::List(val) => PySeries(val).into_py(py),
+            AnyValue::Utf8Owned(val) => val.into_py(py),
+            AnyValue::BinaryOwned(val) => val.into_py(py),
+        }
     }
 }
 
